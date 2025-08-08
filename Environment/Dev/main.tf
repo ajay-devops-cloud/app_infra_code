9ff2ae0a444f48118755a937dba5706207346e33
Ajay Yadav <ajaykumaryadav97@gmail.com>
2025-08-09 00:11:29 +0530

    vm bna rhe hai

---
+++ Environment/Dev/main.tf
@@ -0,0 +1,52 @@
+module "rg" {
+  source = "../../Module/azurerm_resource_group"
+  resource_group_name = "app-dev-rg"
+  location = "eastus"
+}
+module "vnet" {
+    depends_on = [ module.rg ]
+  source = "../../Module/azurerm_vnet"
+  virtual_network_name = "appvnet"
+  address_space = ["10.0.0.0/16"]
+  location = "eastus"
+  resource_group_name = "app-dev-rg"
+}
+module "subnet" {
+    depends_on = [ module.vnet ]
+  source = "../../Module/azurerm_subnet"
+  subnet_name = "appsubnet"
+  resource_group_name = "app-dev-rg"
+  virtual_network_name = "appvnet"
+  address_prefixes = ["10.0.0.0/24"]
+}
+module "publicip" {
+    depends_on = [ module.rg ]
+  source = "../../Module/azurerm_publickip"
+  publicip_name = "mypublicip"
+  resource_group_name = "app-dev-rg"
+  location = "eastus"
+
+}
+module "vm" {
+    depends_on = [ module.subnet ]
+    source = "../../Module/azurerm_vm"
+  nic_name = "mynic"
+  location = "eastus"
+  resource_group_name = "app-dev-rg"
+  virtual_machene_name = "devta"
+  subnet_name = "appsubnet"
+  virtual_network_name = "appvnet"
+  publicip_name = "mypublicip"
+  publisher = "Canonical"
+   offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    image_version = "latest"
+}
+module "bd" {
+    depends_on = [ module.rg ]
+  source = "../../Module/azurerm_DB"
+  sql_server_name = "mysql"
+  resource_group_name = "app-dev-rg"
+  location = "eastus"
+  sql_database_name = "mydb"
+}
---
+++ Environment/Dev/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name = "rg-ajay"
+    storage_account_name = "itsolution"
+    container_name = "nextlevelcontainer"
+    key = "itit.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.38.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+  subscription_id = ""
+}
