3be019747ab8bf0e9d0f91a645ee021dc7198134
Ajay Yadav <ajaykumaryadav97@gmail.com>
2025-08-19 10:09:58 +0530

    acr and aks addedd

---
+++ Module/azurerm_aks/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = "${var.aks_name}-dns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+    depends_on = [
+    azurerm_container_registry.acr,
+    azurerm_kubernetes_cluster.aks
+  ]
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
